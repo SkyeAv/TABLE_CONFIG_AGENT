77c5d156170529c634117d66cb5ad3b6c1f151f6
SkyeAv <skye.a.goetz@gmail.com>
2025-07-29 09:38:39 -0700

    Skye Lane Goetz (added a yaml parser and started on CLI) Tue Jul 29 09:38:39 AM PDT 2025

--- src/table_config_agent/core/yaml_tk.py
+++ src/table_config_agent/core/yaml_tk.py
@@ -1,3 +1,23 @@
 __author__ = "Skye Lane Goetz"
 
+from ruamel.yaml.error import YAMLError
 from ruamel.yaml import YAML
+from pathlib import Path
+
+yaml = YAML()
+
+def load_yaml(file_path: Path) -> Any:
+    posix_file_path: str = file_path.as_posix()
+    try:
+        with file_path.open("r") as f:
+            return yaml.load(f)
+    except FileNotFoundError:
+        msg: str = f"CODE:3 | {posix_file_path} not found"
+        raise RuntimeError(msg)
+    except PermissionError:
+        msg = f"CODE:4 | Permission denied: {posix_file_path}"
+        raise RuntimeError(msg)
+    except YAMLError as e:
+        err: str = str(e)
+        msg = f"CODE:5 | YAML parsing error in {posix_file_path} | {err}"
+        raise RuntimeError(msg)
--- src/table_config_agent/ui/cli.py
+++ src/table_config_agent/ui/cli.py
@@ -1,3 +1,9 @@
 __author__ = "Skye Lane Goetz"
 
 import typer
+
+app = typer.Typer()
+
+def main() -> None:
+    app()
+    return None
